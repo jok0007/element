0a9ee08d624ed747c70617440fe655e520db6d40
Andrey <79100041+jok0007@users.noreply.github.com>
2021-10-25 05:11:31 +0600

    Update modals.js
    Если одно из окон открыто, то другие не открываются по таймеру

--- modals.js
+++ modals.js
@@ -1,9 +1,10 @@
 const modals = () => {
-    function bindModals(triggerSelector, modalSelector, closeSelector) {
+    function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
         const trigger = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
             close = document.querySelector(closeSelector),
-            scroll = caclcScrooll();
+            windows = document.querySelectorAll('[data-modal]'),
+            scroll = calcScroll();
 
         trigger.forEach(item => {
             item.addEventListener('click', (e) => {
@@ -11,57 +12,75 @@ const modals = () => {
                     e.preventDefault();
                 }
 
-                modal.style.display = 'block';
-                //document.body.style.overflow = 'hidden';
-                document.body.classList.add('modal-open');
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
+
+                modal.style.display = "block";
+                document.body.style.overflow = "hidden";
                 document.body.style.marginRight = `${scroll}px`;
             });
         });
 
-        close.addEventListener('click', (e) => {
-            modal.style.display = 'none';
-            //document.body.style.overflow = '';
-            document.body.classList.remove('modal-open');
+        close.addEventListener('click', () => {
+            windows.forEach(item => {
+                item.style.display = 'none';
+            });
+
+            modal.style.display = "none";
+            document.body.style.overflow = "";
             document.body.style.marginRight = `0px`;
         });
 
         modal.addEventListener('click', (e) => {
-            if (e.target === modal) {
-                modal.style.display = 'none';
-                //document.body.style.overflow = '';
-                document.body.classList.remove('modal-open');
+            if (e.target === modal && closeClickOverlay) {
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
+
+                modal.style.display = "none";
+                document.body.style.overflow = "";
                 document.body.style.marginRight = `0px`;
             }
         });
     }
 
     function showModalByTime(selector, time) {
-        setTimeout(() => {
-            document.querySelector(selector).style.display = 'block';
-            document.body.style.overflow = 'hidden';
+        setTimeout(function () {
+            let display;
+
+            document.querySelectorAll('[data-modal]').forEach(item => {
+                if (getComputedStyle(item).display !== 'none') {
+                    display = "block";
+                }
+            });
+
+            if (!display) {
+                document.querySelector(selector).style.display = 'block';
+                document.body.style.overflow = "hidden";
+            }
         }, time);
     }
-    
-    function caclcScrooll() {
+
+    function calcScroll() {
         let div = document.createElement('div');
+
         div.style.width = '50px';
         div.style.height = '50px';
         div.style.overflowY = 'scroll';
         div.style.visibility = 'hidden';
 
         document.body.appendChild(div);
-
         let scrollWidth = div.offsetWidth - div.clientWidth;
         div.remove();
 
         return scrollWidth;
     }
 
-    bindModals('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
-    bindModals('.phone_link', '.popup', '.popup .popup_close');
-    bindModals('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+    bindModal('.button-design', '.popup-design', '.popup-design .popup-close');
+    bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
 
-    //showModalByTime('.popup', 60000);
+    //showModalByTime('.popup-consultation', 5000);
 };
 
 export default modals;
